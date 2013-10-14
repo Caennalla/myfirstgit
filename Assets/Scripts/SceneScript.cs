2ab56130fd9598ba1a562e9248c95b0f2fef7e7e
Juho Roivas <juho-petteri.1.roivas2@edu.karelia.fi>
2013-10-14 10:53:45 +0300

    Adds Title, Results, Gameplay and Credits scenes with limited functionality

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneScript : MonoBehaviour {
+	
+	public string titleText = "Default scene";
+	
+	void OnGUI() {
+	
+	GUI.Box(new Rect(10,10,150,150), titleText);
+		
+	if (GUI.Button(new Rect(20,40,130,20), "GameplayScene")) {
+		Application.LoadLevel("GameplayScene");
+		}	
+		
+	if (GUI.Button(new Rect(20,60,130,20), "ResultsScene")) {
+		Application.LoadLevel("ResultsScene");
+		}
+		
+	if (GUI.Button(new Rect(20,80,130,20), "TitleScene")) {
+		Application.LoadLevel("TitleScene");
+		}
+		
+	if (GUI.Button(new Rect(20,100,130,20), "CreditsScene")) {
+		Application.LoadLevel("CreditsScene");
+		}
+	
+	if (GUI.Button(new Rect(20,120,130,20), "Quit")) {
+		Application.Quit();
+		}
+	}
+}
